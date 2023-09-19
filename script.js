a1a4f08ffdce54df772e7f84f687b98f671690f3
dudufidelis <duduecal12@gmail.com>
2023-09-19 13:38:20 -0300

    Terminado logica do filtro

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ let skateParkTricks = ["Nose Slide", " Tail Slide", "Croked", "Rock Slide", "Man
 let allTricks = soloTricks.concat(skateParkTricks);
 
 
-console.log(gameMode)
-
 document.querySelector('#btn').addEventListener('click', () =>{
     const gameMode = document.querySelector('#gameMode').value;
-     
-    let number = Math.floor(Math.random() * soloTricks.length);
-    const randomTrick = soloTricks[number];
-    const item = document.querySelector('#trick');
-    item.innerHTML = randomTrick;
+    if (gameMode === 'skateParkTricks') {
+        let number = Math.floor(Math.random() * skateParkTricks.length);
+        const randomTrick = skateParkTricks[number];
+        showTrick(randomTrick);
+    } else if (gameMode === 'soloTricks') {
+        let number = Math.floor(Math.random() * soloTricks.length);
+        const randomTrick = soloTricks[number];
+        showTrick(randomTrick);
+    } else {
+        let number = Math.floor(Math.random() * allTricks.length);
+        const randomTrick = allTricks[number];
+        showTrick(randomTrick);
+    }
 });
+
+function showTrick(p) {
+    const trick = document.querySelector('#trick');
+    trick.innerHTML = p;
+}
 
 
 
